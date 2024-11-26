286db3da5c08b41b736b9613086ff6b5c39111c4
Niranjan Ravi <niranjanr5696@gmail.com>
2024-11-26 11:14:49 +0100

    spiral matrix problem variable names changed

--- C++/54_spiral_matrix.cpp
+++ C++/54_spiral_matrix.cpp
@@ -23,51 +23,62 @@ n == matrix[i].length
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        int nrow = matrix.size();
-        int ncol = matrix[0].size();
-        int l_wall = -1, r_wall = ncol, u_wall = 0, d_wall = nrow;
-        char direction = 'r';
-        vector<int> result;
-        int i = 0, j = 0;
-        while (result.size() < nrow * ncol) {
-            result.push_back(matrix[i][j]);
-            if (direction == 'r') {
-                j++;
-                if (j == r_wall) {
-                    r_wall--;
-                    j = r_wall;
-                    direction = 'd';
-                    i++;
+        int row = 0, column = 0;
+        int column_size = matrix[0].size();
+        int row_size = matrix.size();
+        int number_of_elements = 0;
+
+        vector<int> output(column_size * row_size, 0);
+
+        int boundary_left = -1, boundary_right = column_size;
+        int boundary_up = 0, boundary_down = row_size;
+
+        string direction = "right";
+
+
+        while (number_of_elements < column_size * row_size) {
+            //cout << "number_of_elements = " << number_of_elements << endl;
+            //cout << "row = " << row << endl;
+            //cout << "column = " << column << endl;
+            //cout << "direction = " << direction << endl;
+            output[number_of_elements++] = matrix[row][column];
+            if (direction == "right") {
+                ++column;
+                if (column == boundary_right) {
+                    --boundary_right;
+                    --column;
+                    ++row;
+                    direction = "down";
                 }
             }
-            else if (direction == 'd') {
-                i++;
-                if (i == d_wall) {
-                    d_wall--;
-                    i = d_wall;
-                    direction = 'l';
-                    j--;
+            else if (direction == "left") {
+                --column;
+                if (column == boundary_left) {
+                    ++boundary_left;
+                    ++column;
+                    --row;
+                    direction = "up";
                 }
             }
-            else if (direction == 'l') {
-                j--;
-                if (j == l_wall) {
-                    l_wall++;
-                    j = l_wall;
-                    direction = 'u';
-                    i--;
+            else if (direction == "up") {
+                --row;
+                if (row == boundary_up) {
+                    ++boundary_up;
+                    ++row;
+                    ++column;
+                    direction = "right";
                 }
             }
-            else if (direction == 'u') {
-                i--;
-                if (i == u_wall) {
-                    u_wall++;
-                    i = u_wall;
-                    direction = 'r';
-                    j++;
+            else if (direction == "down") {
+                ++row;
+                if (row == boundary_down) {
+                    --boundary_down;
+                    --row;
+                    --column;
+                    direction = "left";
                 }
             }
         }
-        return result;
+        return output;
     }
 };
